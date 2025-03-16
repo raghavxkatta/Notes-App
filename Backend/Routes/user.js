b9bc1093e9ae219f7232a33c24a26b7cda3fb650
Raghav Katta <Raghavkatta2005@gmail.com>
2025-03-16 17:07:45 +0530

    Profile Page authentication with JWT

--- Backend/Routes/user.js
+++ Backend/Routes/user.js
@@ -1,22 +1,82 @@
 import express from 'express'
 const router=express.Router()   
 import User from '../models/User.js'
-
+import {jwtAuthMidddleware,generateToken} from '../jwt.js'  
 
 /* Signup Route will handle user registrations and issue a JWT token upon successfull registration */
 router.post('/signup', async (req, res) => {
 try{
 const{username,email,password}=req.body
-const newUser=new User({
-    username,
-    email,
-    password
-})
-await newUser.save()
+const newUser=new User({username,email,password})
+
+/* Save the user to the database */
+const response=await newUser.save()
+console.log("response saved")
+
+/* Generate Payload */
+const payload={
+    id:response._id,
+    username:response.username,
+    email:response.email
+}
+console.log(JSON.stringify(payload))
+
+/* Generate JWT Token */
+const token=generateToken(response)
+console.log("token is ",token)      
+return res.status(201).json({message:'User Created Successfully',token:token})
 }
 catch(err){
     res.status(400).json({message:'Error creating user', error:err.message})
 }})
 
+/* Function to generate a JWT token */
+const generateToken=(userData)=>{
+    /* Generate a JWT token */
+    /* User ki id ko payload mein daal diya */
+    /* JWT_SECRET ko secret key ke role mein use kiya hai */
+    return jwt.sign({id:userData._id},process.env.JWT_SECRET)
+}
 
+
+
+// LOGIN ROUTE
+router.post('/login', async (req, res) => {
+    try{
+        const{username,password}=req.body
+        const user = await User.findOne({username:username})
+        /* Agar iss username db mein nhi hoga toh */
+        if(!user||!await user.comparePassword(password)){
+            return res.status(401).json({message:'Invalid Username or Password'})   
+        }
+
+        // Generate Token
+        const payload={
+            username:user.username,
+            email:user.email  
+        }
+        const token=generateToken(user)
+        /* Return token as a response */
+        res.json({token})
+    }
+catch(err){
+    res.status(400).json({message:'Error logging in', error:err.message})
+}   
+})
+
+// PROFILE ROUTE
+// This route will be protected by JWT token
+router.get('/profile',jwtAuthMidddleware,async(req,res)=>{
+    try{
+        const userData=req.jwtpayload
+        console.log(userData)
+
+        const userId=userData.id
+        const user = await User.findById(userId)
+        res.status(200).json({user})
+    }
+    catch(err){
+        res.status(400).json({message:'Error fetching user profile', error:err.message})
+    }   
+})
 export default router
--- Backend/jwt.js
+++ Backend/jwt.js
@@ -1,17 +1,36 @@
-const jwt = require('jsonwebtoken');
+// Client (Postman / React App) API pe request bhejta hai aur JWT token bhejta hai.
+//  Yeh middleware header se JWT token uthata hai.
+//  Agar token nahi mila toh 401 Unauthorized error dega.
+//  Agar token mila toh verify karega.
+//  Agar token valid hai toh req.jwtPayload mein user ka data store karega.
+//  Agar token invalid hai toh 401 Invalid Token error dega.
+//  Agar sab kuch sahi hai toh next() call hoga aur request aage ke route pe chali jayegi.
+//  Agar user login hota hai toh generateToken() function JWT token generate karega.
 
+
+import jwt from 'jsonwebtoken';
+
+/* yeh middleware function check karta hai ki incoming request valid JWT token hai ki nhi  */
 const jwtAuthMidddleware=(req,res,next)=>{
+
+    /* Checks if the request header contains the Authorization header */
+    /* Agar request header mein authorization header nahi hai ya agar authorization header mein bearman se start nahi ho raha hai toh unauthorized error return karenge */
+    if (!req.headers.authorization || !req.headers.authorization.startsWith("Bearer ")) {
+        return res.status(401).json({ error: 'Unauthorized: No Token Provided' });
+    }
+
     
     /* Extract the JWT token from the request header */
     /* Humne Postman api se bearman method ke through bearman token header mein pass kardiya */
     /* split karne se bearer upar chale jaayega aur token 1 mein aajayega */
+    /* Toh hum sirf actual token le rhe Bearer hata kar */
 const token =req.headers.authorization.split(' ')[1]
 /* agar token nahi mila */
 if(!token) return res.status(401).json({error:'Unauthorized'}) 
 
     /* agar token mil gaya toh verify karenge */ 
 try{
-/* Verify The JWT token */
+/* Verify The JWT token and token ke andar jo payload hai usko bhi extract karta hai */
 const decoded=jwt.verify(token,process.env.JWT_SECRET)
 
 /* Attach User information to the request object */
@@ -26,7 +45,7 @@ catch(err){
 
 
 // Generate JWT Token
-const generateToken=(user)=>{
+const generateToken=(user)=>{   
     /* Generate a JWT token */
     /* User ki id ko payload mein daal diya */
     /* JWT_SECRET ko secret key ke ro
